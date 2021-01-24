6244dba31071cb16bdbc3475b1ab7414595e664d
anayensi <vnvyensi@gmail.com>
2021-01-24 12:21:35 -0500

    commit tree 6e680b0d8fa94816f8435222f4f40b2ad9e344d7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+    def initialize(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def roster(roster)
+        @roster = roster
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(add_student)
+        @add_student = add_student
+    end
+
+    def add_student
+        @add_student
+    end
+end
+
+school = School.new("Test School")
